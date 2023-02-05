e5ed9129fd259d3e0d528020915e93733d053dc9
chankaran2996 <45528864+chankaran2996@users.noreply.github.com>
2023-02-05 22:57:11 +0530

    Weight to Pounds

---
+++ index.js
@@ -0,0 +1,11 @@
+const cal = () =>{
+    const weigth = document.getElementById("Weight").value;
+    document.getElementById("Weight").value="";
+    if(weigth==""){
+        alert("Enter weigth vaule");
+        return;
+    }
+    let pound = weigth*2.2046;
+    document.getElementById("output").innerHTML="Value of "+weigth+" is "+pound;
+    console.log(weigth,pound)
+}
